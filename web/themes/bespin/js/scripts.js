21562aad6d40a81a84718ffb9abf36743589f26a
Jason Thompson <jthomps86@gmail.com>
2018-07-25 09:07:03 -0500

    create bespin theme

---
+++ web/themes/bespin/js/scripts.js
@@ -0,0 +1,32 @@
+(function($, Drupal, drupalSettings) {
+  $(document).ready(function () {
+    //
+    // var $header = $("header"),
+    //   $clone = $header.before($header.clone().addClass("cloned"));
+    //
+    // $(window).on("scroll", function() {
+    //   var fromTop = $(window).scrollTop();
+    //   $("body").toggleClass("sticky", (fromTop > 100));
+    // });
+
+    // Move video modals
+    // $(".modal").appendTo("body");
+  });
+
+
+    // Stop video when modal closed
+    $(document).ready(function () {
+      // $('.modal').each(function () {
+      //   var src = $(this).find('iframe').attr('src');
+      //
+      //   $(this).on('click', function () {
+      //
+      //     $(this).find('iframe').attr('src', '');
+      //     $(this).find('iframe').attr('src', src);
+      //
+      //   });
+      // });
+    });
+
+
+})(jQuery);
